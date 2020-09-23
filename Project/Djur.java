ca6afe7815529faeb1c2e50797a4c7ff116c19b6
Coin-byte <71173059+Coin-byte@users.noreply.github.com>
2020-09-23 11:51:25 +0200

    Add files via upload

--- Project/Djur.java
+++ Project/Djur.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Djur {
+
+    private int x_pos;
+    private int y_pos;
+    //private int speed;
+    private boolean alive;
+
+    public Djur(){
+
+        x_pos = 10;
+        y_pos = 10;
+        //speed = 1;
+        alive =true;
+    }
+    public Djur(int speed){
+        this.x_pos = (int) (0 + Math.random() * 20);
+        this.y_pos = (int) (0 + Math.random() * 60);
+        //this.speed = speed;
+        alive = true;
+    }
+
+    @Override
+    public String toString() {
+        return "x_pos=" + x_pos +
+                ", y_pos=" + y_pos +
+                //", speed=" + speed +
+                ", alive=" + alive;
+    }
 }
