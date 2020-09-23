d56d875570da4d2f5540132d3fa61b1fafefcd5b
oskand01 <58656584+oskand01@users.noreply.github.com>
2020-09-23 23:43:10 +0200

    Add files via upload

---
+++ Gepard.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Gepard extends Djur {
+
+    private boolean hungry;
+
+    public Gepard() {
+        setSpeed(1); //Bara tillfälliga värden
+        hungry = true;
+
+    }
+
+    public void hunt() {
+        setSpeed(3); //Går lätt att ändra med setSpeed
+    }
+
+
+
+
+    @Override
+    public String toString() {
+
+        return "Gepard\n" + super.toString() + "\n";
+    }
+}
