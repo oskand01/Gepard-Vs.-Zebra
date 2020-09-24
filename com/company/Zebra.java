0e50f4eb00de3d530d1a58d645250171294d4405
elivilhelm <71629392+elivilhelm@users.noreply.github.com>
2020-09-24 17:18:45 +0200

    Update Zebra.java

--- com/company/Zebra.java
+++ com/company/Zebra.java
@@ -7,6 +7,7 @@ public class Zebra extends Djur {
     public Zebra(){
         scared = false;
         setSpeed(2);
+        setDirectionRandom(directionRandom);
     }
 
     public void createZebras() {
